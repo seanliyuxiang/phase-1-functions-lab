6c5113d370cd78161c6db69c230ea1b8ae50101f
Yuxiang Li <seanliyuxiang@gmail.com>
2021-08-30 20:42:20 -0500

    commit tree bdbc0a8a6b8e7524d16088aa742df2909e051f2b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(beginningBlock, destinationBlock) {
+  return Math.abs(beginningBlock - destinationBlock) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let fare;
+  let distanceInFeet = distanceTravelledInFeet(start, destination);
+
+  if (distanceInFeet <= 400) {
+    fare = 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    fare = (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+    fare = 25;
+  } else if (distanceInFeet >= 2500) {
+    fare = 'cannot travel that far';
+  }
+
+  return fare;
+}
